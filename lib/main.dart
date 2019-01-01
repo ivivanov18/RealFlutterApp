16ea86e9681fbd0749c44bd7f3e6529bbe215503
ivivanov18 <ivanov.ivan.v@gmail.com>
2019-01-01 20:54:27 +0200

    feat: add possibility to fetch data from API

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import "dart:convert";
+
 
 void main() => runApp(RealWorldApp());
 
@@ -13,6 +16,20 @@ class RealWorldState extends State<RealWorldApp>{
 
   var _isLoading = true;
 
+  _fetchData() async{
+    print("Attempting to load data...");
+    final url = "https://api.letsbuildthatapp.com/youtube/home_feed";
+    final response = await http.get(url);
+    if(response.statusCode == 200){
+
+      final mapResults = json.decode(response.body);
+      final videosJSON = mapResults["videos"];
+      videosJSON.forEach((video) {
+        print(video["name"]);
+      });
+
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,10 +42,11 @@ class RealWorldState extends State<RealWorldApp>{
             IconButton(
               icon: Icon(Icons.refresh),
               onPressed: (){
-                print("Reloading...");
                 setState((){
                   _isLoading = !_isLoading;
                 });
+                _fetchData();
+
               },
             )
           ],
